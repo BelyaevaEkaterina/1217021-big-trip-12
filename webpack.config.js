df60b71e2ea958ce42a53b239cd92f744f6ef57a
BelyaevaEkaterina <56511688+BelyaevaEkaterina@users.noreply.github.com>
2020-08-27 08:45:20 +0400

    Разбивка на модули

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
 
-module.export = {
+module.exports = {
   mode: 'development',
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public'),
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+  }
 };
